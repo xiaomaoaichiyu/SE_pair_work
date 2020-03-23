254276ff808c10cec72f53d5d69fc633c03a9354
yanhuaqaq <wuzhaobang@buaa.edu.cn>
2020-03-23 23:11:35 +0800

    add UI

---
+++ UI/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    class Class1
+    {
+        [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void getPoint(double[] x, double[] y, int[] size);
+
+        [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void addLine(char l, int x1, int y1, int x2, int y2);
+
+        [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void delLine(char l, int x1, int y1, int x2, int y2);
+
+        [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void addCircle(int x, int y, int r);
+
+
+        [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void delCircle(int x, int y, int r);
+
+        [DllImport("core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int getResultOfIntersect();
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void AddLine(int x1, int y1, int x2, int y2);
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void RemoveLine(int x1, int y1, int x2, int y2);
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void AddRay(int x1, int y1, int x2, int y2);
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void RemoveRay(int x1, int y1, int x2, int y2);
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void AddSection(int x1, int y1, int x2, int y2);
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void RemoveSection(int x1, int y1, int x2, int y2);
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void AddCircle(int x, int y, int r);
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void RemoveCircle(int x, int y, int r);
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int GetIntersectionsSize();
+
+        [DllImport("_core.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void GetIntersections(double[] xs, double[] ys, int size);
+
+
+
+    }
+}
---
+++ UI/Intersect.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Intersect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.文件导入 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(2, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(952, 479);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "请输入要添加的几何对象:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "添加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 文件导入
+            // 
+            this.文件导入.Location = new System.Drawing.Point(31, 23);
+            this.文件导入.Name = "文件导入";
+            this.文件导入.Size = new System.Drawing.Size(92, 45);
+            this.文件导入.TabIndex = 4;
+            this.文件导入.Text = "文件导入";
+            this.文件导入.UseVisualStyleBackColor = true;
+            this.文件导入.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(213, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(210, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(215, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "请选择要删除的几何对象:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(445, 76);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "删除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(581, 33);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 35);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "绘制";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(714, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 18);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "查看交点坐标:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(717, 44);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(222, 26);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(714, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 18);
+            this.label4.TabIndex = 11;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 585);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.文件导入);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form5";
+            this.Text = "Intersect";
+            this.Load += new System.EventHandler(this.Form5_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button 文件导入;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
